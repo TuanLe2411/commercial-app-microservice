0dd5eff126d928660c16d3d71eb6489e4632ea15
TuanLe2411 <tuanldhaihaua@gmail.com>
2023-10-27 00:40:45 +0700

    add base service

---
+++ es_event_sync_service/src/main/java/com/commercial/app/es_event_sync_service/EsEventSyncServiceApplication.java
@@ -0,0 +1,13 @@
+package com.commercial.app.es_event_sync_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsEventSyncServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsEventSyncServiceApplication.class, args);
+	}
+
+}
---
+++ es_event_sync_service/src/test/java/com/commercial/app/es_event_sync_service/EsEventSyncServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.commercial.app.es_event_sync_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsEventSyncServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
