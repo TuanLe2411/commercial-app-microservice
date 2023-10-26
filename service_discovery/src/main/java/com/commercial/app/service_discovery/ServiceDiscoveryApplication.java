6f18135af6e3039d36b56327ebd1506d3266e676
TuanLe <49343179+TuanLe2411@users.noreply.github.com>
2023-10-26 22:32:58 +0700

    add product service and shop service with load balance

--- service_discovery/src/main/java/com/commercial/app/service_discovery/ServiceDiscoveryApplication.java
+++ service_discovery/src/main/java/com/commercial/app/service_discovery/ServiceDiscoveryApplication.java
@@ -7,9 +7,8 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @SpringBootApplication
 @EnableEurekaServer
 public class ServiceDiscoveryApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(ServiceDiscoveryApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceDiscoveryApplication.class, args);
+    }
 
 }
